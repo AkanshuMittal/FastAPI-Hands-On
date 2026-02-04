82ef372184ce2e237e9d3f80324d5d118e6fc2d5
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-04 23:49:09 +0530

    Modify the create_post code and write better version

--- app/main.py
+++ app/main.py
@@ -90,7 +90,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     #                 (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
