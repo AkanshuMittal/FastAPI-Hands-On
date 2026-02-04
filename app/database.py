5e422080a866980eb83e4ca98867c00a1d6242de
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-05 01:38:03 +0530

    Update the database_url

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@ip-address/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:akanshu2307%40%23@localhost/fastapi'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:akanshu2307%40%23@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
