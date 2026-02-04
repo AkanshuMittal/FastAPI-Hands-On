8262dab11da03481dd7b84e85c13a8c2e059d397
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-05 00:15:37 +0530

    Delete the post using id

--- app/main.py
+++ app/main.py
@@ -134,14 +134,19 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 ##  Delete the post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
 
-    if deleted_post == None:
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-
+    
+    post.delete(synchronize_session=False)
+    db.commit()
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 #update the post
