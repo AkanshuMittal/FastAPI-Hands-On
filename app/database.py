d342d23a2659263e304b5ef372fbb4ffc30734f0
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-10 01:16:13 +0530

    Update file

--- app/database.py
+++ app/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@ip-address/hostname>/<database_name>'
-#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:akanshu2307%40%23@localhost/fastapi'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@ip-address/hostname>/<database_name>' 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
