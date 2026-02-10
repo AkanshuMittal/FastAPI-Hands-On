4d784983917ee2f407bc8c51263bb9db9bd25b33
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-10 20:56:59 +0530

    Adding functionality for converting the password into hash format

---
+++ app/utils.py
@@ -0,0 +1,7 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes = ["bcrypt"], deprecated="auto")
+
+def hash(password: str) -> str:
+   password_bytes = password.encode("utf-8")[:72]
+   return pwd_context.hash(password_bytes.decode("utf-8", errors="ignore"))
