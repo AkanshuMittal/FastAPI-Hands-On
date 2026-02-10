1ec3a4d9d58fe8e8d80851ef30c2130ce5fec334
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-10 21:14:57 +0530

    Creating path operation for getting the user details from the DB

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schema
+from . import models, schema, utils
 from .database import engine, get_db
 from typing import List
 
@@ -174,9 +174,23 @@ def update_post(id: int, updated_post: schema.PostCreate, db: Session = Depends(
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schema.UserOut)
 def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    # hash the password -- 
+    hashed_passowrd = utils.hash(user.password)
+    user.password = hashed_passowrd
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     
     return new_user
+
+
+@app.get("/users/{id}", response_model=schema.UserOut)
+def get_user(id: int,db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                              detail=f"user with id: {id} was not found")
+    
+    return user
