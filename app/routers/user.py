1142dd40107381f30e526552e77c6fab3a555c88
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-11 00:35:39 +0530

    In the routers for the user adding prefix = /users for clean version of code

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,7 +8,7 @@ router = APIRouter(
     tags = ['Users']
 )
 
-@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schema.UserOut)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schema.UserOut)
 def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
     # hash the password -- 
     hashed_passowrd = utils.hash(user.password)
@@ -21,7 +21,7 @@ def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/users/{id}", response_model=schema.UserOut)
+@router.get("/{id}", response_model=schema.UserOut)
 def get_user(id: int,db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     
