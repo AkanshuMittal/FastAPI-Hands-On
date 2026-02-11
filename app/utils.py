18f407710128990ee2f67c58742b0aeb89be4a0b
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-11 23:37:19 +0530

    Defining function for verify the user password from the database

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,6 @@ pwd_context = CryptContext(schemes = ["bcrypt"], deprecated="auto")
 def hash(password: str) -> str:
    password_bytes = password.encode("utf-8")[:72]
    return pwd_context.hash(password_bytes.decode("utf-8", errors="ignore"))
+
+def verify(plain_password, hashed_password):
+   return pwd_context.verify(plain_password, hashed_password)
