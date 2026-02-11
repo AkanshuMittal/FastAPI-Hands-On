073ac60b1d929faa3a64b31229fa8958c5c60462
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-11 23:39:42 +0530

    Adding include_router for authentication

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 from . import models, schema, utils
 from .database import engine, get_db
 from typing import List
-from .routers import post, user
+from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -39,3 +39,4 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
