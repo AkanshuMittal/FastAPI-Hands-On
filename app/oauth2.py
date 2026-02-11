74b77b4eebc3b01c7f081e44987eb8817d7ee64e
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-12 00:01:42 +0530

    Creating access token in the login function with the help of oauth2.py file

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 #Algorithm
 #Expiration time
 
-
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4ca6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
