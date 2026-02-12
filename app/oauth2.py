1113ad6d5d659cfdc9051951d3c02b6458ac50bb
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-12 23:59:53 +0530

    Defining function for getting the user id

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schema
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
 #SECRET_KEY
 #Algorithm
@@ -34,3 +38,9 @@ def verify_access_token(token: str, credentials_exception):
         
     except JWTError:
         raise credentials_exception
+    
+    
+def get_current_user(token: str = Depends(oauth2_schema)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WW-Authenticate":"Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
