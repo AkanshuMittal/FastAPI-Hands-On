3caec1f22407e53d8e785fbff19da6357cf9068a
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-12 23:53:21 +0530

    Defining function for verifying the access token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,11 +1,12 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schema
 
 #SECRET_KEY
 #Algorithm
 #Expiration time
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4ca6cf63b88e8d3e7"
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -19,3 +20,17 @@ def create_access_token(data: dict):
     
     return encoded_jwt
     
+def verify_access_token(token: str, credentials_exception):
+    
+    try:
+        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    
+        id: str = payload.get("user_id")
+    
+        if id is None:
+            raise credentials_exception
+        token_data = schema.TokenData(id=id)
+        
+    except JWTError:
+        raise credentials_exception
