c13762324cfd78637df5a1fed1fad5adfa2591d1
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-20 22:13:27 +0530

    Updating the code

--- app/oauth2.py
+++ app/oauth2.py
@@ -10,14 +10,13 @@ oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 #Algorithm
 #Expiration time
 
-
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
